f5c1f9cf157cafdae43d35f1bd408dc0e3508908
hanfengya1 <eyou2024@163.com>
2025-06-18 01:06:31 +0800

    Site updated: 2025-06-18 01:06:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/15/这是一篇新的博文/","2025/06/15/My-New-Post/","2025/06/15/hello-world/"];function toRandomPost(){
+var posts=["undefined/永洪bi相关解决方案/","undefined/FineBi相关解决方案/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
