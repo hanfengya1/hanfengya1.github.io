354891803e3411f8eecad3b5f597eb0660efc4cf
hanfengya1 <eyou2024@163.com>
2025-06-18 12:24:48 +0800

    Site updated: 2025-06-18 12:24:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["undefined/永洪bi相关解决方案/","undefined/FineBi相关解决方案/"];function toRandomPost(){
+var posts=["undefined/FineBi相关解决方案/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
